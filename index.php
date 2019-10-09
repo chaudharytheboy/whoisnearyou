a3554c8ce7b5be0d6168617dd4930fcee96f71bf
santosh <a@b.com>
2019-10-09 19:04:42 +0530

    initial-commit

---
+++ index.php
@@ -0,0 +1,565 @@
+<!doctype html>
+<html class="no-js" lang="en"> 
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>DATING</title>
+	<meta name="keywords" content="Pneuservis Praha, autoservis Praha" />
+	<meta name="description" content="Náš pneuservis v Praze vám nabízí přezutí pneu a uskladnění pneu. Připravujeme pro Vás také autservis v Praze s pravidelnými servisními prohlídkami." />
+	<meta name="author" content="inCUBE interactive" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	<meta name="viewport" content="width=1280" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/stylelogin.css" />
+	<script src="js/modernizr-2.5.2-respond-1.1.0.min.js"></script>
+</head>
+<body>
+	<script>
+		var www_path = "/";
+		var cdn_path = "https://ce86eef19092c75b6ba1-4eea68cbac810806818cca4da6deda74.ssl.cf3.rackcdn.com/";
+		var latitude = 50.097383;
+		var longitude = 14.481509;
+		</script>
+	
+	<div id="loader">		
+		<div class="rotate-canvas">
+			<div class="top-side"><div class="bg"></div><div class="logo"><span class="back"></span><span></span><span class="hover"></span></div></div>
+			<div class="bottom-side"><div class="slider"><span class="percentage"></span></div></div>
+		</div>
+		<div class="container">
+			<div class="left-side">
+
+				<span class="mask-right">
+				<span class="sign-wrap">
+					<span class="mask-right">
+					<span class="sign">
+						<span class="mask-right">
+						<span class="tit">
+							WHO IS
+							<!-- <span>servis bez front</span> -->
+						</span>
+						</span>
+					</span>
+					</span>
+				</span>
+				</span>
+
+			</div>
+
+			<div class="right-side">
+				
+				<span class="mask-left">
+				<span class="sign-wrap">
+					<span class="mask-left">
+					<span class="sign">
+						<span class="mask-left">
+						<span class="tit">
+							NEAR YOU
+							<!-- <span>fastservis bez front</span> -->
+						</span>
+						</span>
+					</span>
+					</span>
+				</span>
+				</span>
+				
+			</div>
+		</div>
+	</div>
+
+	<div id="wrap">
+
+		<div id="header-container">
+			<header>
+				<h1 id="title">
+					<a class="logo ir" href="">
+						servis, fastservis Praha						<span></span>
+						<span class="hover"></span>
+					</a>
+				</h1>
+				<nav>
+					<ul>
+						<li>
+							<a rel="home" href="#">
+								<span class="grid"></span>
+								<span class="hover"><span></span></span>
+								<span class="text">Mingle Chamber</span>
+							</a>
+						</li>
+						<li>
+							<a rel="left" href="#">
+								<span class="grid"></span>
+								<span class="hover"><span></span></span>
+								<span class="text">Table For Two</span>
+							</a>
+						</li>
+						<li>
+							<a rel="right" href="#">
+								<span class="grid"></span>
+								<span class="hover"><span></span></span>
+								<span class="text">Single's bar</span>
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</div>
+
+		<div id="main-container">
+			<div id="main">
+
+				<div class="background-sky">
+					<div class="left"><img src="img/sky.jpg"></div>
+					<div class="right"><img src="img/sky.jpg"></div>
+				</div>
+				<div class="background-clouds">
+					<div class="left"><img src="img/clouds.png"></div>
+					<div class="right"><img src="img/clouds.png"></div>
+				</div>
+				<div class="trees">
+					<div class="tree-left"></div>
+					<div class="tree-right"></div>
+					<div class="bonsai-left"></div>
+					<div class="bonsai-right"></div>
+				</div>
+				
+				<div class="background-middle">
+					
+					<div class="pavement"></div><div class="hedge-left"></div><div class="hedge-right"></div>
+
+					<div class="building">
+						<div class="left"></div>
+						<div class="front">
+							<div class="partrait-pneu"></div>
+							<div class="partrait-auto"></div>
+							<p>- SERVIS -</p>
+							<div class="flowerpot-left"></div>
+							<div class="flowerpot-right"></div>
+						</div>
+						<div class="right"></div>
+						<div class="top-left"></div><div class="top-front"></div><div class="top-right"></div>
+					</div>
+					<div class="building-left">
+						<div class="left"></div>
+						<div class="front">
+							<div class="tree"></div>
+							<div class="partrait-pneu"></div><p>Who is </p>
+						</div>
+						<div class="right"></div>
+						<div class="top-front"></div><div class="top-right"></div><div class="rail"></div>
+					</div>
+					<div class="building-right">
+						<div class="left"></div>
+						<div class="front">
+							<div class="tree"></div>
+							<div class="partrait-auto"></div><p>Near You</p>
+						</div>
+						<div class="right"></div>
+						<div class="top-left"></div><div class="top-front"></div>
+					</div>
+				</div>
+
+				<div class="left-scene">
+
+					<div class="garage">
+						<a href="#" class="logo">
+							<span></span>
+							<span class="hover"></span>
+						</a>
+						<div class="gate">
+							<p class="drag-advance">
+								<span>TAHEM</span>
+								 I am good
+							</p>
+							<div class="bubble">
+								<h4>SLEVA</h4>
+								<p>How are you?</p>
+							</div>
+						</div>
+						<div class="shadow"></div>
+					</div>
+
+					<div class="car">
+						<span class="hover"></span>
+
+						<p>
+							<span class="mask-right">
+							<span class="sign-wrap">
+								<span class="mask-right">
+								<span class="sign">
+									<span class="mask-right">
+									<span class="tit">
+										Better half
+										<span>Love gives you everything</span>
+									</span>
+									</span>
+								</span>
+								</span>
+							</span>
+							</span>
+						</p>
+					</div>
+
+					<!-- Note remove-->
+					<article class="terms-content" style = "display:none;">
+						<section>
+							<header>
+								<h2>OBCHODNÍ PODMÍNKY</h2>
+							</header>
+							<p>
+Poskytnutím kontaktních údajů v&nbsp;objednávkovém formuláři zákazník souhlasí se zařazením do databáze společnosti HMR system, s.r.o., která je provozovatelem servisu HMR. 
+</p>
+<br/><br/>
+<p>
+Tato databáze bude využita pouze v souladu s provozem činností výše uvedené společnosti a nebude poskytnuta třetím osobám. Nakládání s těmito daty je v souladu se zákonem č. 480/2004 Sb. a č. 138/2002 Sb.
+</p>
+<br/><br/>
+<p>
+Platnost veškerých voucherů a slevových kupónů je časově omezena s&nbsp;vyznačením doby platnosti a veškeré služby musí být čerpány nejpozději v&nbsp;den ukončení platnosti!
+</p>
+<br/><br/>
+<p>
+Vydavatelem voucherů a slevových kupónů je pouze provozovatel, společnost HMR system, s.r.o., IČO 25685686, která zajišťuje jejich evidenci čerpání a platnost. Provozovatel si vyhrazuje právo odmítnout plnění v&nbsp;případě zjištění neplatnosti nebo duplicity voucheru nebo slevového kupónu. Platnost a finanční plnění voucherů a slevových kupónů je právně nevymahatelné.
+</p>
+<br/><br/>
+<p class="red">
+Všechna práva vyhrazena<br/>HMR system, s.r.o.
+</p>
+							<a href="#">
+								<span class="hover"></span>
+								<span class="close-icon"></span>
+								<span class="text">ZAVŘÍT</span>
+							</a>
+						</section>
+					</article>
+
+					<article>
+						<header>
+							<div></div>
+							<h2><strong>LOGIN HERE</strong></h2>
+							<p><span class="bold">Welcome back,</span> <span class="bold2"> Login to your account to continue.</span> </p>
+						</header>
+						<div class="row dt_login login">
+						<form method="POST" action="/Useractions/login" class="login">
+							<div class="alert alert-success" role="alert" style="display:none;"></div>
+							<div class="alert alert-danger" role="alert" style="display:none;"></div>
+							<div class="row">
+								<div class="input-field">
+									<label for="username" class="active">Username or Email</label>
+									<input name="username" id="usernamename" type="text" class="validate" required="" autofocus="">									
+								</div>
+							</div>
+							<div class="row">
+								<div class="input-field">
+									<label for="password">Password</label>
+									<input name="password" id="password" type="password" class="validate" required="">									
+								</div>
+							</div>
+							<div class="dt_login_footer valign-wrapper">
+								<button class="btn btn-large waves-effect waves-light bold btn_primary btn_round" type="submit" name="action"><span>Login</span> <svg viewBox="0 0 19 14" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="currentColor" d="M18.6 6.9v-.5l-6-6c-.3-.3-.9-.3-1.2 0-.3.3-.3.9 0 1.2l5 5H1c-.5 0-.9.4-.9.9s.4.8.9.8h14.4l-4 4.1c-.3.3-.3.9 0 1.2.2.2.4.2.6.2.2 0 .4-.1.6-.2l5.2-5.2h.2c.5 0 .8-.4.8-.8 0-.3 0-.5-.2-.7z"></path></svg></button>
+								<a href="" data-ajax="" class="grey-text text-darken-3">Forgot Password?</a>
+							</div>
+
+							<div class="dt_social_login"></div>
+						</form>
+					</div>
+					</article>
+
+					<div class="thanks"></div>
+					
+				</div>
+
+				<div class="middle-scene">
+
+					<a href="#" class="car-pneuservis">
+						<span class="hover"></span>
+
+						<p>
+							<span class="mask-right">
+							<span class="sign-wrap">
+								<span class="mask-right">
+								<span class="sign">
+									<span class="mask-right">
+									<span class="tit">
+										LOGIN
+										<span>login here</span>
+									</span>
+									</span>
+								</span>
+								</span>
+							</span>
+							</span>
+						</p>
+
+					</a>
+					<a  href="#" class="car-autoservis">
+						<span class="hover"></span>
+						<p>
+							<span class="mask-left">
+							<span class="sign-wrap">
+								<span class="mask-left">
+								<span class="sign">
+									<span class="mask-left">
+									<span class="tit">
+											SIGN UP
+											<span>sign up here</span>
+									</span>
+									</span>
+								</span>
+								</span>
+							</span>
+							</span>
+						</p>
+					</a> 
+					
+					<div class="drag-separator"></div>
+					<div class="dragger">
+						<span class="bg"></span>
+						<span></span>
+						<span class="hover"></span>
+						<span class="animated-round"></span>
+					</div>
+
+				</div>
+
+				<div class="right-scene">
+					<div class="garage">
+						<a href="#" class="logo">
+							<span></span>
+							<span class="hover"></span>
+						</a>
+						<div class="gate">
+							<p class="drag-advance">
+								<span>TAHEM</span>
+								Thank you
+							</p>
+							<div class="bubble">
+								<h4>SLEVA</h4>
+								<p>Hey,Looking nice </p>
+							</div>
+						</div>
+						<div class="shadow"></div>
+					</div>
+					
+					<div class="car">
+						<span class="hover"></span>
+						<p>
+							<span class="mask-left">
+							<span class="sign-wrap">
+								<span class="mask-left">
+								<span class="sign">
+									<span class="mask-left">
+									<span class="tit">
+											SOULMATE
+											<span>Soul are eternal</span>
+									</span>
+									</span>
+								</span>
+								</span>
+							</span>
+							</span>
+						</p>
+					</div>
+
+					<article class="terms-content">
+						<section>
+							<header>
+								
+								<h2>OBCHODNÍ PODMÍNKY</h2>
+							</header>
+							<a href="#"><span class="hover"></span><span class="text">ZAVŘÍT</span></a>
+						</section>
+					</article>
+
+					<article>
+						<header>
+							<div></div>
+							<h2><strong>REGISTER HERE</strong></h2>
+							<p><span class="bold">Get started,</span> <span class="bold2">Sign up to get started finding your partner!</span></p>
+						</header>
+						
+						<div class="row dt_login" style="margin-bottom: 50px;">
+							<form method="POST" action="/Useractions/register" class="register">
+								<div class="alert alert-success" role="alert" style="display:none;"></div>
+								<div class="alert alert-danger" role="alert" style="display:none;"></div>
+								<div class="row">
+									<div class="input-field col s6">
+										<label for="first_name" class="active">First Name</label>
+										<input name="first_name" id="first_name" type="text" class="validate" value="" autofocus="">										
+									</div>
+									<div class="input-field col s6">
+										<label for="last_name">Last Name</label>
+										<input name="last_name" id="last_name" type="text" class="validate" value="">
+									</div>
+								</div>
+								<div class="row">
+									<div class="input-field col s6">
+										<label for="username">Username</label>
+										<input name="username" id="username" type="text" class="validate" value="" required="">
+									</div>
+									<div class="input-field col s6">
+										<label for="email">Email</label>
+										<input name="email" id="email" type="email" class="validate" value="" required="">
+									</div>
+								</div>
+								<div class="row">
+									<div class="input-field col s6">
+										<label for="password">Password</label>
+										<input name="password" id="passwordsss" type="password" class="validate" value="" required="">
+									</div>
+									<div class="input-field col s6">
+										<label for="c_password">Confirm Password</label>
+										<input name="c_password" id="c_password" type="password" class="validate" value="" required="">
+									</div>
+								</div>
+								<label class="terms_check">
+									<input type="checkbox" style="opacity: 1 !important;display: block;">
+									<span>By creating your account, you agree to our 
+										<a href="http://suntekitsolutions.com/nearyou/terms" data-ajax="/terms">Terms of Use</a> &amp; <a href="http://suntekitsolutions.com/nearyou/privacy" data-ajax="/privacy">Privacy Policy</a></span>
+								</label>
+								<div class="dt_login_footer valign-wrapper">
+									<button class="btn btn-large waves-effect waves-light bold btn_primary btn_round" id="sign_submit" type="submit" disabled=""><span>Register</span> <svg viewBox="0 0 19 14" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="currentColor" d="M18.6 6.9v-.5l-6-6c-.3-.3-.9-.3-1.2 0-.3.3-.3.9 0 1.2l5 5H1c-.5 0-.9.4-.9.9s.4.8.9.8h14.4l-4 4.1c-.3.3-.3.9 0 1.2.2.2.4.2.6.2.2 0 .4-.1.6-.2l5.2-5.2h.2c.5 0 .8-.4.8-.8 0-.3 0-.5-.2-.7z"></path></svg></button>
+								</div>
+							</form>
+						</div>
+						<br><br>
+					</article>
+					<div class="thanks"></div>
+				</div>
+
+				<div class="layer5">
+					<div class="tree-left"></div>
+					<div class="tree-right"></div>
+					<div class="bike"></div>
+				</div>
+				<div class="layer4">
+					<div class="man-left"></div>
+					<div class="man-right"></div>
+				</div>
+				<div class="layer3">
+					<div class="lamp-left"></div>
+					<div class="lamp-right"></div>
+				</div>
+				<div class="layer2">
+					
+					<div class="bush-right">
+						<div class="contact">
+							<h4>Well,</h4>
+							<p>
+								dating has <br />become a sport and
+								<br /><br /><br />
+								not about finding <br />the person you love.
+							</p>
+						</div>
+					</div>
+
+					<div class="rock-right"></div>
+				</div>
+				
+				<aside class="news">
+					
+					<h2>Menu</h2>
+					<div class="bg"><span></span></div>
+					<div class="hover"></div>
+					<p>
+						<span class="text">start exploring Who Near You for dating</span>
+					</p>
+				</aside>
+
+			</div> <!-- #main -->
+
+		</div> <!-- #main-container -->
+
+		<div id="help" style = "display:none;">
+						
+			<div class="question-mark">
+				<div>?<span></span></div>
+			</div>
+
+			<div class="controll-detail">
+				<h3>OVLÁDÁNÍ ŠIPKAMI</h3>
+				<p>
+					Pro pohyb mezi jednotlivými
+					stránkami můžete použít také
+					<strong>šipky</strong> na vaší klávesnici.
+				</p>
+				<div class="arrows"></div>
+			</div>
+
+			<div class="controll-detail">
+				<h3>MENU + OVLÁDÁNÍ MYŠÍ</h3>
+				<p>
+					<strong>Menu</strong> naleznete vlevo pod logem,
+					Pro pohyb po webu lze použít také
+					<strong>skrolování</strong>. pomocí myše.
+				</p>
+				<div class="menu"></div>
+				<div class="plus"><span class="vertical"></span><span class="horizontal"></span></div>
+				<div class="mouse"></div>
+			</div>
+		</div>
+
+		<div id="incube-hover">
+			<div class="rotate-canvas">
+				<div class="top-side">
+					<div class="container">
+						<h1 style="font-size: 250px;color: #fff;margin-top: -100px;">near</h1>
+					</div>
+				</div>
+				<div class="bottom-side">
+					<div class="container">
+						<h1 style="font-size: 250px;color: #000;margin-top: -100px;margin-left: 64%;">you</h1>
+						
+					</div>
+					<div class="slider"></div>
+				</div>
+			</div>
+		</div>
+		
+
+		<div id="footer-container">
+			<footer>
+				<!-- freespace -->
+				<div class="freespace"></div>
+				
+				<!-- creator -->
+				<div class="incube"><a href="#"><p>created by account interactive</p><span></span></a></div>
+				<!-- copyrights -->
+				<div class="rights">copyrights 2019 Nearyou</div>
+				<!-- social networks -->
+				<div class="soc">
+					<div class="twitter"></div>
+					<div class="facebook"></div>
+					<div class="google-plus"></div>
+				</div>
+				<!-- help -->
+				<div class="help"><a href="#">?<span class="hover">?</span></a></div>
+
+				<div style="float:none;clear:both;border:0;padding:0;"></div>
+			</footer>
+		</div>
+
+	</div>
+
+<!-- <script src="http://maps.google.com/maps/api/js"></script> -->
+<!-- <script src="https://maps.googleapis.com/maps/api/js?v=3&key="></script> -->
+<script src="js/js.js"></script>
+<script src="js/script.included.js"></script>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30561459-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</body>
+</html>
